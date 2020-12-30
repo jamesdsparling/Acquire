31603e8893cb7471dfa721d4e798904d2700c424
James Sparling <jamesdsparling@gmail.com>
2020-12-30 23:50:24 +0000

    Initial Commit

---
+++ Shared/AcquireApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcquireApp.swift
+//  Shared
+//
+//  Created by James Sparling on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct AcquireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
